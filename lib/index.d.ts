97f19e1b8481bcdee29a4ecab249297cb7b6215f
Julian Speckmann <176780813+KingIronMan2011@users.noreply.github.com>
2025-12-10 19:00:29 +0100

    feat: add version check before npm publish and improve code formatting
    - Implemented a check to see if the package version is already published on npm before attempting to publish.
    - Added conditional steps in the GitHub Actions workflow to skip publishing if the version is already published.
    - Improved code formatting in TypeScript definition files and JavaScript files for better readability and consistency.
    - Updated the style injection function to handle cases where the document is undefined.

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,19 +2,19 @@ import { ITerminalAddon, Terminal } from '@xterm/xterm';
 import { ISearchOptions, SearchAddon } from '@xterm/addon-search';
 import './index.css';
 export interface SearchBarOption extends ISearchOptions {
-    searchAddon: SearchAddon;
+  searchAddon: SearchAddon;
 }
 export declare class SearchBarAddon implements ITerminalAddon {
-    private readonly options;
-    private terminal;
-    private readonly searchAddon;
-    private searchBarElement;
-    private searchKey;
-    constructor(options: Partial<SearchBarOption>);
-    activate(terminal: Terminal): void;
-    dispose(): void;
-    show(): void;
-    hidden(): void;
-    private on;
-    addNewStyle(newStyle: string): void;
+  private readonly options;
+  private terminal;
+  private readonly searchAddon;
+  private searchBarElement;
+  private searchKey;
+  constructor(options: Partial<SearchBarOption>);
+  activate(terminal: Terminal): void;
+  dispose(): void;
+  show(): void;
+  hidden(): void;
+  private on;
+  addNewStyle(newStyle: string): void;
 }
